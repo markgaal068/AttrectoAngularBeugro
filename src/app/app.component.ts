cfe4a6ce7dacd64392f57bdc5dd0361b716c767d
markgaal068 <gaalmark.markit@gmail.com>
2024-05-19 10:43:42 +0200

    END

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,20 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
 import { SandboxComponent } from './pages/sandbox/sandbox.component';
 import { NavbarComponent } from './pages/navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './pages/movies/movies/movies.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, SandboxComponent, NavbarComponent, RouterLink, HomeComponent, MoviesComponent, FormsModule],
+  imports: [RouterOutlet, ButtonComponent, SandboxComponent, NavbarComponent, RouterLink, MoviesComponent, FormsModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-
-  title = 'internship-angular-test-2024';
-
-  //Második feladat
-  onButtonClick(): void {
-    console.log('Gomb klikkelve!');
-  }
 }
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SandboxComponent } from './pages/sandbox/sandbox.component';
-import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './pages/movies/movies/movies.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 export const routes: Routes = [
-{'path' : '', component:HomeComponent},
+{ path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: 'home', component: HomeComponent },
 {'path':  'movies', component:MoviesComponent},
 {'path': 'sandbox', component:SandboxComponent}
 
